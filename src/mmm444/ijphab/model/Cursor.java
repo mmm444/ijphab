69426c8b13a52d9ab42b8dbf13fd97635ce96d3b
Michal Rydlo <michal.rydlo@gmail.com>
2018-01-19 00:23:41 +0100

    Add code style and inspection settings
    They are taken from the idea-ce project. Incorectly formatted
    code and some inspection warnings are resolved.

--- src/mmm444/ijphab/model/Cursor.java
+++ src/mmm444/ijphab/model/Cursor.java
@@ -4,6 +4,7 @@ package mmm444.ijphab.model;
  * Cursor field used for paging in {@code .search} method results.
  */
 public class Cursor {
+  @SuppressWarnings("unused")
   private String after;
 
   public String getAfter() {
--- src/mmm444/ijphab/model/MethodResponse.java
+++ src/mmm444/ijphab/model/MethodResponse.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.tasks.impl.RequestFailedException;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.List;
 
 /**
@@ -13,33 +12,33 @@ import java.util.List;
  */
 @SuppressWarnings({"CanBeFinal", "unused"})
 public abstract class MethodResponse {
-    @SerializedName("error_code")
-    private String errorCode;
-    @SerializedName("error_info")
-    private String errorInfo;
+  @SerializedName("error_code")
+  private String errorCode;
+  @SerializedName("error_info")
+  private String errorInfo;
 
-    public String getErrorCode() {
-        return errorCode;
+  public String getErrorCode() {
+    return errorCode;
+  }
+
+  public String getErrorInfo() {
+    return errorInfo;
+  }
+
+  /**
+   * Validate the data returned from the server. Return the list of found validation issues.
+   */
+  @Nonnull
+  abstract List<String> validate();
+
+  public void checkOk() {
+    if (StringUtil.isNotEmpty(errorInfo)) {
+      throw new RequestFailedException(errorInfo);
     }
 
-    public String getErrorInfo() {
-        return errorInfo;
+    List<String> validationErrs = validate();
+    if (!validationErrs.isEmpty()) {
+      throw new RequestFailedException("Invalid data received: " + StringUtil.join(validationErrs, "\n"));
     }
-
-    /**
-     * Validate the data returned from the server. Return the list of found validation issues.
-     */
-    @Nonnull
-    abstract List<String> validate();
-
-    public void checkOk() {
-        if (StringUtil.isNotEmpty(errorInfo)) {
-            throw new RequestFailedException(errorInfo);
-        }
-
-        List<String> validationErrs = validate();
-        if (!validationErrs.isEmpty()) {
-            throw new RequestFailedException("Invalid data received: " + StringUtil.join(validationErrs, "\n"));
-        }
-    }
+  }
 }
